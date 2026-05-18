7f504efa2a32e98ea50ae9adc4b229083aa665b3
saha-7op <saha7pritam@gmail.com>
2026-05-18 17:10:16 +0530

    new urls added

--- src/urls.js
+++ src/urls.js
@@ -12,11 +12,17 @@
 
 const STORES = [
   {
-    name  : 'primeabgb',
-    parser: require('./parsers/primeabgb'),
+    name: "primeabgb",
+    parser: require("./parsers/primeabgb"),
     categories: [
-      { slug: 'cpu-processor', url: 'https://www.primeabgb.com/buy-online-price-india/cpu-processor/' },
-      // { slug: 'ram-memory',    url: 'https://www.primeabgb.com/buy-online-price-india/ram-memory/' },
+      {
+        slug: "cpu-processor",
+        url: "https://www.primeabgb.com/buy-online-price-india/cpu-processor/",
+      },
+      {
+        slug: "ram-memory",
+        url: "https://www.primeabgb.com/buy-online-price-india/ram-memory/",
+      },
       // { slug: 'motherboards',  url: 'https://www.primeabgb.com/buy-online-price-india/motherboards/' },
       // { slug: 'graphic-cards', url: 'https://www.primeabgb.com/buy-online-price-india/graphic-cards-gpu/' },
       // { slug: 'hdd',           url: 'https://www.primeabgb.com/buy-online-price-india/internal-hard-drive/' },
@@ -24,12 +30,15 @@ const STORES = [
   },
 
   {
-    name  : 'mdcomputers',
-    parser: require('./parsers/mdcomputers'),
+    name: "mdcomputers",
+    parser: require("./parsers/mdcomputers"),
     categories: [
-      { slug: 'cpu-processor', url: 'https://mdcomputers.in/catalog/processor' },
+      {
+        slug: "cpu-processor",
+        url: "https://mdcomputers.in/catalog/processor",
+      },
       // { slug: 'motherboards',  url: 'https://mdcomputers.in/catalog/motherboard' },
-      // { slug: 'ram-memory',    url: 'https://mdcomputers.in/catalog/ram' },
+      { slug: "ram-memory", url: "https://mdcomputers.in/catalog/ram" },
     ],
   },
 
@@ -38,7 +47,7 @@ const STORES = [
   //   parser: require('./parsers/pickpcparts'),
   //   categories: [
   //     { slug: 'cpu-processor', url: 'https://pickpcparts.in/processors/' },
-  //     // { slug: 'ram-memory',    url: 'https://pickpcparts.in/rams/' },
+  //     { slug: 'ram-memory',    url: 'https://pickpcparts.in/rams/' },
   //     // { slug: 'motherboards',  url: 'https://pickpcparts.in/motherboards/' },
   //     // { slug: 'graphic-cards', url: 'https://pickpcparts.in/graphics_cards/' },
   //     // { slug: 'storages',      url: 'https://pickpcparts.in/storages/' },
@@ -48,35 +57,45 @@ const STORES = [
   // },
 
   {
-  name  : 'vedant',
-  parser: require('./parsers/vedant'),
-  categories: [
-    { slug: 'cpu-processor', url: 'https://www.vedantcomputers.com/pc-components/processor' },
-  ],
-},
+    name: "vedant",
+    parser: require("./parsers/vedant"),
+    categories: [
+      {
+        slug: "cpu-processor",
+        url: "https://www.vedantcomputers.com/pc-components/processor",
+      },
+      {
+        slug: "ram-memory",
+        url: "https://www.vedantcomputers.com/pc-components/memory",
+      },
+    ],
+  },
 
-{
-  name  : 'vishal',
-  parser: require('./parsers/vishal'),
-  categories: [
-    {
-      slug: 'cpu-processor',
-      url : 'https://vishalperipherals.com/collections/processors'
-    },
-  ],
-},
+  {
+    name: "vishal",
+    parser: require("./parsers/vishal"),
+    categories: [
+      {
+        slug: "cpu-processor",
+        url: "https://vishalperipherals.com/collections/processors",
+      },
+      {
+        slug: "ram-memory",
+        url: "https://vishalperipherals.com/collections/ram",
+      },
+    ],
+  },
 
-
-// {
-//   name  : 'computechstore',
-//   parser: require('./parsers/computechstore'),
-//   categories: [
-//     {
-//       slug: 'cpu-processor',
-//       url : 'https://computechstore.in/product-category/processor'
-//     },
-//   ],
-// },
+  // {
+  //   name  : 'computechstore',
+  //   parser: require('./parsers/computechstore'),
+  //   categories: [
+  //     {
+  //       slug: 'cpu-processor',
+  //       url : 'https://computechstore.in/product-category/processor'
+  //     },
+  //   ],
+  // },
 
   // To add a new store Example:
   // {
@@ -88,28 +107,30 @@ const STORES = [
   // },
 
   {
-  name  : 'pcstudio',
-  parser: require('./parsers/pcstudio'),
-  categories: [
-    {
-      slug: 'cpu-processor',
-      url : 'https://www.pcstudio.in/product-category/processor/'
-    },
-  ],
-},
+    name: "pcstudio",
+    parser: require("./parsers/pcstudio"),
+    categories: [
+      {
+        slug: "cpu-processor",
+        url: "https://www.pcstudio.in/product-category/processor/",
+      },
+      {
+        slug: "ram-memory",
+        url: "https://www.pcstudio.in/product-category/ram/",
+      },
+    ],
+  },
 
-
-// {
-//   name  : 'elitehubs',
-//   parser: require('./parsers/elitehubs'),
-//   categories: [
-//     {
-//       slug: 'cpu-processor',
-//       url : 'https://elitehubs.com/collections/processor'
-//     },
-//   ],
-// },
-
+  // {
+  //   name  : 'elitehubs',
+  //   parser: require('./parsers/elitehubs'),
+  //   categories: [
+  //     {
+  //       slug: 'cpu-processor',
+  //       url : 'https://elitehubs.com/collections/processor'
+  //     },
+  //   ],
+  // },
 ];
 
 module.exports = { STORES };
